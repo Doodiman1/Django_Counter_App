0d46d83d83e11be3f63c85d4aa8e76d449cafd6a
perry <perry@kali.com>
2023-08-28 21:33:19 +0100

    Counter to track how many times the website has been loaded

--- visitCounter/views.py
+++ visitCounter/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F
 from .models import Visit
 # Create your views here.
 
 def index(request):
-    visitor = Visit.objects.all()
+    visitor = Visit.objects.filter(username='Test')
+    visitor.update(count=F("count") + 1)
     context = { "visitor": visitor }
     return render(
             request, 
