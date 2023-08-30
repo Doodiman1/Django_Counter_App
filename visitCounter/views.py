98b2ed76adb710f375fb5972a77c02ffd41d3819
perry <perry@kali.com>
2023-08-30 21:24:26 +0100

    Simple CSS added

--- visitCounter/views.py
+++ visitCounter/views.py
@@ -1,10 +1,12 @@
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
 from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from .models import Visit
 # Create your views here.
 
 def index(request):
-    visitor = Visit.objects.filter(username='Test')
+    visitor = Visit.objects.filter(username = 'Test')
     visitor.update(count=F("count") + 1)
     context = { "visitor": visitor }
     return render(
@@ -12,3 +14,4 @@ def index(request):
             "visitCounter/index.html",
             context
             )
+
