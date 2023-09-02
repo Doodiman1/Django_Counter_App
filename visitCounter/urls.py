d9cb44b1064526aa525ead5c0a0bc15938829c21
perry <perry@kali.com>
2023-09-02 01:48:52 +0100

    Create User page added. Auth Model module imported. link to create_user page  added to index page.

--- visitCounter/urls.py
+++ visitCounter/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
         path("", views.index, name = "index"),
         path("accounts/", include('django.contrib.auth.urls')),
+        path("create_user", views.createUser, name = "create_user")
         ]
--- visitCounter/views.py
+++ visitCounter/views.py
@@ -2,16 +2,25 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate
 from django.shortcuts import render, get_object_or_404
 from django.db.models import F
+from django.contrib.auth.models import User
 from .models import Visit
 # Create your views here.
 
 def index(request):
     visitor = Visit.objects.filter(username = 'Test')
     visitor.update(count=F("count") + 1)
-    context = { "visitor": visitor }
+    context = { "visitor" : visitor }
     return render(
             request, 
             "visitCounter/index.html",
             context
             )
 
+def createUser(request):
+    #newUser = User.objects.create_user()
+    #context = {"user" : user }
+    return render(
+            request,
+            "registration/new_user.html",
+            #context
+            )
