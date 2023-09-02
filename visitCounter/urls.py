8dd8232b30f403c58abc846a451638cff434112e
perry <perry@kali.com>
2023-09-02 12:59:37 +0100

    Sign Up page replaced with Generic View, Template changed to reflect this

--- visitCounter/urls.py
+++ visitCounter/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
         path("", views.index, name = "index"),
         path("accounts/", include('django.contrib.auth.urls')),
-        path("create_user", views.createUser, name = "create_user")
+        path("create_user/", views.createUser.as_view(), name = "create_user")
         ]
--- visitCounter/views.py
+++ visitCounter/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
 from django.contrib.auth import authenticate
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.db.models import F
-from django.contrib.auth.models import User
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from .models import Visit
-# Create your views here.
+
 
 def index(request):
     visitor = Visit.objects.filter(username = 'Test')
@@ -16,11 +17,8 @@ def index(request):
             context
             )
 
-def createUser(request):
-    #newUser = User.objects.create_user()
-    #context = {"user" : user }
-    return render(
-            request,
-            "registration/new_user.html",
-            #context
-            )
+class createUser(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/create_user.html"
+
